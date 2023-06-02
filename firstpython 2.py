b80167a84cf19d4a8788115b19938fdb1f6d4e1a
nadiyetou <nadia2nass@gmail.com>
2023-06-02 16:19:02 -0400

    just add more future to my project

--- firstpython 2.py
+++ firstpython 2.py
@@ -1,77 +1,75 @@
-# Sample data dictionary.
-sample_data = {
-    "One Piece": {
-        "synopsis": "Gol D. Roger was known as the Pirate King...",
-        "episodes": 1000,
-        "rating": 8.56
-    },
-    "Naruto": {
-        "synopsis": "Naruto Uzumaki, a mischievous adolescent ninja...",
-        "episodes": 720,
-        "rating": 8.19
-    },
-    "Attack on Titan": {
-        "synopsis": "Several hundred years ago, humans were nearly exterminated by Titans...",
-        "episodes": 75,
-        "rating": 8.49
-    },
-    "Death Note": {
-        "synopsis": "Light Yagami, an intelligent high school student...",
-        "episodes": 37,
-        "rating": 8.66
-    },
-    "Fullmetal Alchemist: Brotherhood": {
-        "synopsis": "Edward and Alphonse Elric's reckless pursuit of...",
-        "episodes": 64,
-        "rating": 9.22
-    },
-    "Dragon Ball Z": {
-        "synopsis": "Goku and his friends defend the Earth against villains...",
-        "episodes": 291,
-        "rating": 8.31
-    },
-    "Bleach": {
-        "synopsis": "Ichigo Kurosaki gains the abilities of a Soul Reaper...",
-        "episodes": 366,
-        "rating": 7.92
-    },
-    "My Hero Academia": {
-        "synopsis": "In a world where most of the population has superpowers...",
-        "episodes": 113,
-        "rating": 8.24
-    }
-    # Add more anime entries here
-}
+# This is a list of dictionaries where each dictionary represents an anime and its details.
+anime_list = [
+    {"name": "Naruto", 
+     "genre": "Action, Adventure, Fantasy", 
+     "episodes": 220,
+     "summary": "Naruto follows the journey of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village."},
+    
+    {"name": "Death Note", 
+     "genre": "Mystery, Psychological thriller", 
+     "episodes": 37,
+     "summary": "Death Note is about a high school student who discovers a supernatural notebook that allows him to kill anyone by writing the victim's name while picturing their face. The story follows his attempts to become a god by creating a New World cleansed of evil, using the notebook."},
+    
+    {"name": "Attack on Titan", 
+     "genre": "Action, Post-apocalyptic", 
+     "episodes": 75,
+     "summary": "In a world where humanity is threatened by gigantic humanoid creatures known as Titans, a young man named Eren Yeager joins the military with his friends to fight for survival."},
+     
+    {"name": "One Piece", 
+     "genre": "Adventure, Fantasy", 
+     "episodes": 1000,
+     "summary": "One Piece follows the adventures of Monkey D. Luffy and his pirate crew in order to find the greatest treasure ever left by the legendary Pirate, Gold Roger. The famous mystery treasure named 'One Piece'."},
+     
+    {"name": "Fullmetal Alchemist: Brotherhood", 
+     "genre": "Action, Adventure, Dark fantasy", 
+     "episodes": 64,
+     "summary": "The story of two brothers, Edward and Alphonse Elric, who are searching for a Philosopher's Stone to restore their bodies after a failed attempt to bring their mother back to life using Alchemy."}
+]
 
-# Function to get anime information
-def get_anime_info(anime_name):
-    if anime_name in sample_data:
-        anime = sample_data[anime_name]
-        title = anime_name
-        synopsis = anime["synopsis"]
-        episodes = anime["episodes"]
-        rating = anime["rating"]
+# Function to search for anime in the list and print its details.
+def search_anime(anime_name):
+    # Loop through each anime in the list.
+    for anime in anime_list:
+        # Check if the name of the anime matches the name entered by the user.
+        if anime["name"].lower() == anime_name.lower():
+            # If a match is found, print the details of the anime and return from the function.
+            print(f"\nAnime name: {anime['name']}\nGenre: {anime['genre']}\nNumber of episodes: {anime['episodes']}\nSummary: {anime['summary']}\n")
+            return
 
-        # Printing the information
-        print("Title:", title)
-        print("Synopsis:", synopsis)
-        print("Episodes:", episodes)
-        print("Rating:", rating)
-    else:
-        print("No anime found with that name.")
+    # If no match is found, print a message indicating that the anime was not found.
+    print(f"\nNo anime found with the name: {anime_name}\n")
 
-# Main program
-def main():
-    while True:
-        print("==== Anime Information App ====")
-        print("Enter the name of an anime (or 'q' to quit):")
-        anime_name = input("> ")
+print(""" $$$$$$\            $$\                               $$\   $$\                 $$\ $$\           
+$$  __$$\           \__|                              $$$\  $$ |                $$ |\__|          
+$$ /  $$ |$$$$$$$\  $$\ $$$$$$\$$$$\   $$$$$$\        $$$$\ $$ | $$$$$$\   $$$$$$$ |$$\  $$$$$$\  
+$$$$$$$$ |$$  __$$\ $$ |$$  _$$  _$$\ $$  __$$\       $$ $$\$$ | \____$$\ $$  __$$ |$$ | \____$$\ 
+$$  __$$ |$$ |  $$ |$$ |$$ / $$ / $$ |$$$$$$$$ |      $$ \$$$$ | $$$$$$$ |$$ /  $$ |$$ | $$$$$$$ |
+$$ |  $$ |$$ |  $$ |$$ |$$ | $$ | $$ |$$   ____|      $$ |\$$$ |$$  __$$ |$$ |  $$ |$$ |$$  __$$ |
+$$ |  $$ |$$ |  $$ |$$ |$$ | $$ | $$ |\$$$$$$$\       $$ | \$$ |\$$$$$$$ |\$$$$$$$ |$$ |\$$$$$$$ |
+\__|  \__|\__|  \__|\__|\__| \__| \__| \_______|      \__|  \__| \_______| \_______|\__| \_______|""")
 
-        if anime_name == "q":
-            break
+print("Anime")
+print("\nKonichiwa! Welcome to AnimeFinder, where you get info about your favorite animes. Let's start the journey...\n")
 
-        get_anime_info(anime_name)
-        print()
+# Repeat this block until the user wants to quit.
+while True:
+    # Prompt the user to enter the name of an anime.
+    anime_name = input("Enter the name of an anime: ")
 
-# Calling the main program
-main()
+    # Call the search_anime function with the name entered by the user.
+    search_anime(anime_name)
+
+    # Ask the user if they want to continue or quit the program.
+    continue_choice = input("Would you like to search for another anime? (yes/no): ")
+    if continue_choice.lower() != 'yes':
+        break
+
+print("""
+ /\_/\  
+( o.o ) 
+ > ^ <  
+ /\_/\  
+( o.o ) 
+ > ^ <  
+""")
+print("Arigato for using AnimeFinder! Hope you had a fantastic anime journey. Sayonara and see you next time!")
